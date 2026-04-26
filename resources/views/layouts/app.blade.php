f91b368d74bc32eb839e4762559267520e575ef9
fpendii <nurefendi210203@gmail.com>
2026-04-26 08:58:55 +0700

    update halaman barang masuk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <title>@yield('title', 'Dashboard') - Pengelolaan Data</title>
     <link rel="icon" type="image/png" href="/image/icon.png">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     @vite(['resources/js/app.js'])
